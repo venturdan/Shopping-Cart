d8086f53058076db0a789380bf6cef124e164e3b
venturdan <venturdan@outlook.com.br>
2023-03-22 19:39:02 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,74 +1,15 @@
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions.js';
-import { createProductElement } from './helpers/shopFunctions.js';
-import { fetchProduct } from './helpers/fetchFunctions.js';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+
 import './style.css';
 
-function mostrarCarregando() {
-  const loadingElement = document.createElement('div');
-  loadingElement.classList.add('loading');
-  loadingElement.textContent = 'Carregando...';
-  document.body.appendChild(loadingElement);
-}
-
-function esconderCarregando() {
-  const loadingElement = document.querySelector('.loading');
-  if (loadingElement) {
-    document.body.removeChild(loadingElement);
-  }
-}
-
-const productsSection = document.querySelector('.products');
-
-mostrarCarregando();
-
-fetchProductsList('computador')
-  .then((products) => {
-    products.forEach((product) => {
-      const productElement = createProductElement(product);
-      productsSection.appendChild(productElement);
-    });
-    esconderCarregando();
-  })
-  .catch((error) => {
-    console.error(error);
-    esconderCarregando();
-  });
-
-function showError(message) {
-  const errorElement = document.createElement('div');
-  errorElement.textContent = message;
-  errorElement.classList.add('error');
-  document.body.appendChild(errorElement);
-}
-
-function hideError() {
-  const errorElement = document.querySelector('.error');
-  if (errorElement) {
-    errorElement.remove();
-  }
-}
-
-async function fetchProduct(productId) {
-  if (!productId) {
-    throw new Error('ID não informado');
-  }
-
-  try {
-    mostrarCarregando();
-
-    const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
-    const data = await response.json();
-
-    esconderCarregando();
-    hideError();
-
-    return data;
-  } catch (error) {
-    hideLoading();
-    showError('Algum erro ocorreu, recarregue a página e tente novamente');
-    throw error;
-  }
-}
-
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const productsContainer = document.querySelector('.products');
+
+const response = await fetchProductsList('computador');
+
+response.forEach((result) => {
+  const resultElement = createProductElement(result);
+  productsContainer.appendChild(resultElement);
+});
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,49 +4,7 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it("fetchProductsList is a function", () => {
-    expect(typeof fetchProductsList).toBe("function");
-  });
-
-  it("should call fetch when executing fetchProductsList with 'computador'", async () => {
-    window.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue({}),
-    });
-
-    await fetchProductsList("computador");
-
-    expect(window.fetch).toHaveBeenCalled();
-  });
-
-  it("should call fetch with correct URL when executing fetchProductsList with 'computador'", async () => {
-    const expectedUrl = `${defaultEndpoint}/search?q=computador`;
-    window.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue({}),
-    });
-
-    await fetchProductsList("computador");
-
-    expect(window.fetch).toHaveBeenCalledWith(expectedUrl);
-  });
-
-  it("should return an object with the correct properties when executing fetchProductsList with 'computador'", async () => {
-    window.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue(computadorSearch),
-    });
-
-    const result = await fetchProductsList("computador");
-
-    expect(result).toEqual(computadorSearch);
-  });
-
-  it("should return an error message when executing fetchProductsList without an argument", async () => {
-    const errorMessage = "Termo de busca não informado";
-    const expectedError = new Error(errorMessage);
-
-    try {
-      await fetchProduct();
-    } catch (error) {
-      expect(error).toEqual(expectedError);
-    }
+  it('...', () => {
+    
   });
 });
